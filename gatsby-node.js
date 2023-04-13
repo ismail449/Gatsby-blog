170026930b1eed139d45fa877a716dfd93fad095
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-04-14 01:53:30 +0200

    added all the code for the gatsby blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,10 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
@@ -15,4 +18,38 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+  return graphql(`
+    query MyQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(results => {
+    results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
